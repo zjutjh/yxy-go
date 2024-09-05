b93f611ddad617ad1d03cd8d22ef646a5bfce095
XiMo-210 <2831802697@qq.com>
2024-09-06 01:56:57 +0800

    feat(login): add logic to get security token

---
+++ internal/consts/consts.go
@@ -0,0 +1,11 @@
+package consts
+
+const (
+	APP_ALL_VERSION = "6.2.8"
+	APP_VERSION     = "611"
+	COMPUS_URL      = "https://compus.xiaofubao.com"
+)
+
+const (
+	GET_SECURITY_TOKEN_URL = COMPUS_URL + "/common/security/token"
+)
--- internal/logic/login/getSecurityTokenLogic.go
+++ internal/logic/login/getSecurityTokenLogic.go
@@ -2,9 +2,13 @@ package login
 
 import (
 	"context"
+	"fmt"
 
+	"yxy-go/internal/consts"
 	"yxy-go/internal/svc"
 	"yxy-go/internal/types"
+	"yxy-go/internal/utils/yxyClient"
+	"yxy-go/pkg/xerr"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -13,6 +17,16 @@ type GetSecurityTokenLogic struct {
 	logx.Logger
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
+}
+
+type GetSecurityTokenYxyResp struct {
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
+	Data       struct {
+		Level         uint8  `json:"level"`
+		SecurityToken string `json:"securityToken"`
+	} `json:"data"`
+	Success bool `json:"success"`
 }
 
 func NewGetSecurityTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetSecurityTokenLogic {
@@ -24,7 +38,21 @@ func NewGetSecurityTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *GetSecurityTokenLogic) GetSecurityToken(req *types.GetSecurityTokenReq) (resp *types.GetSecurityTokenResp, err error) {
-	// todo: add your logic here and delete this line
+	yxyReq, yxyHeaders := yxyClient.GetYxyBaseReqParam(req.DeviceID)
+	yxyReq["sceneCode"] = "app_user_login"
 
-	return
+	var yxyResp GetSecurityTokenYxyResp
+	r, err := yxyClient.HttpSendPost(consts.GET_SECURITY_TOKEN_URL, yxyReq, yxyHeaders, &yxyResp)
+	if err != nil {
+		return nil, err
+	}
+
+	if yxyResp.StatusCode != 0 {
+		return nil, xerr.WithCode(xerr.ErrUnknown, fmt.Sprintf("yxy response: %v", r))
+	}
+
+	return &types.GetSecurityTokenResp{
+		Level:         yxyResp.Data.Level,
+		SecurityToken: yxyResp.Data.SecurityToken,
+	}, nil
 }
